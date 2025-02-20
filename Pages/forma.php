dc09252c74461e6f866afad8fd673bea4ffa6876
Tat-T <ring_87_87@mail.ru>
2025-02-20 16:49:01 +0300

    first commit

---
+++ Pages/forma.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Регистрация</title>
+</head>
+
+<body class="bg-warning-subtle">
+    <div class="container mt-5 ">
+        <form method="POST" action="register.php" class="container bg-light-subtle col-6 rounded p-3 mt-3 position-absolute top-50 start-50 translate-middle shadow-lg">
+            <div class="row mb-3">
+                <h2 class="text-warning">Форма регистрации</h2>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="Login" class="form-control border border-warning focus-ring focus-ring-warning" id="floatingLogin" placeholder="Password">
+                <label for="floatingLogin">Login</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="Password" class="form-control border border-warning focus-ring focus-ring-warning" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Password</label>
+            </div>
+            <div class="form-floating">
+                <input type="email" class="form-control border border-warning focus-ring focus-ring-warning" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Email address</label>
+            </div>
+            <div class="d-grid mt-3 bg-warning-subtle rounded-3">
+                <button type="submit" class="btn btn-outline-warning text-dark w-100 rounded p-3 ">Зарегистрироваться</button>
+            </div>
+        </form>
+    </div>
+
+</body>
+
+</html>
---
+++ README.md
@@ -0,0 +1,7 @@
+<img src="image/Screenshot_1.png" alt="скрин" width="350">
+<img src="image/Screenshot_2.png" alt="скрин" width="350">
+<img src="image/Screenshot_3.png" alt="скрин" width="350">
+
+## Запуск и установка
+
+http://localhost/php_dz4/Pages/forma.php
